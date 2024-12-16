2f8e8595d303c95d13449b9569c772168b01f211
Federico Putamorsi <47032913+refedico@users.noreply.github.com>
2024-12-16 15:50:46 +0100

    Update seg_model.py

--- seg_model.py
+++ seg_model.py
@@ -4,9 +4,9 @@ from ultralytics import YOLO
 #model = YOLO("yolo11n-seg.pt")
 
 # Train the model
-#results = model.train(data="configmodel.yaml", epochs=1, imgsz=640)
+results = model.train(data="configmodel.yaml", epochs=1, imgsz=640)
 
-model = YOLO("best.pt")
+#model = YOLO("best.pt")
 
 
-results = model("D:/Desktop/Adas_test/ds_segmentation/images/img6.jpg")
+#results = model("D:/Desktop/Adas_test/ds_segmentation/images/img6.jpg")
